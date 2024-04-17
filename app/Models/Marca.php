af0d9f4339cf5ec8c890b547520845de6934fdaf
unknown <arthurcavani@gmail.com>
2024-04-16 21:52:01 -0300

    Validacao de dados no endpoint

--- app/Models/Marca.php
+++ app/Models/Marca.php
@@ -9,4 +9,21 @@ class Marca extends Model
 {
     use HasFactory;
     protected $fillable = ['nome', 'imagem'];
+
+    public function rules()
+    {
+        return [
+            'nome' => 'required|unique:marcas,nome,'.$this->id.'|min:3',
+            'imagem' => 'required'
+        ];
+    }
+
+    public function feedback()
+    {
+        return [
+            'required' => 'O campo :attribute é obrigatório',
+            'nome.unique' => 'Nome da marca já existe',
+            'nome.min' => 'Nome deve possuir no mínimo 3 caracteres'
+        ];
+    }
 }
