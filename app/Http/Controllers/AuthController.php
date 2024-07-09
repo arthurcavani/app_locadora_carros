721a23f967decf92b4d0bd57eec9f960329c2310
Arthur <arthurcavani@gmail.com>
2024-07-09 12:01:35 -0300

    Implementacao de metodos do token jwt

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -17,14 +17,16 @@ class AuthController extends Controller
     }
 
     public function logout(){
-        return 'logout';
+        auth('api')->logout();
+        return response()->json(['msg' => 'Logout realizado com sucesso']);
     }
 
     public function refresh(){
-        return 'refresh';
+        $token = auth('api')->refresh();
+        return response()->json(['token' => $token]);
     }
 
     public function me(){
-        return 'me';
+        return response()->json(auth()->user());
     }
 }
